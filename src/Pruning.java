82b93a475d1dd78569c47af68fbff3f2b5bb2210
Younies Mahmoud <younies.mahmoud@gmail.com>
2016-08-08 10:03:26 -0500

    adding the correct pathes to the files

--- src/Pruning.java
+++ src/Pruning.java
@@ -2,9 +2,9 @@ import java.io.FileNotFoundException;
 
 public class Pruning {
 
-	static String path_to_the_tree_nodes = "/Users/youniesmahmoud/Desktop/newTestIds.txt";
+	static String path_to_the_tree_nodes = "/export1/project/hondius/newKrakenResearch/databases/nodes.txt";
 	static String path_to_the_needed_nodes  = "/export1/project/hondius/newKrakenResearch/databases/uids_new.txt"; 
-	static String output_file_path = "/Users/youniesmahmoud/Desktop/newTestIds2.txt";
+	static String output_file_path = "/export1/project/hondius/newKrakenResearch/nodes222.txt";
 	static TreePruning tree ;
 	public static void main(String[] args) throws FileNotFoundException
 	{
